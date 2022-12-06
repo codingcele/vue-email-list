0ef736feec15ea1974d99e468b4044e5895e6b07
codingcele <marcocelegato9@gmail.com>
2022-12-06 16:47:42 +0100

    tentativo

---
+++ main.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue;
+
+    createApp({
+        data() {
+            return {
+                mails : []
+            }
+        },
+        methods : {
+
+        },
+        mounted() {
+            i = 0;
+            while (i < 5) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((result) => {
+                    this.mails.push(result.data.response);
+                });
+                i++;
+            }
+        }
+    }).mount("#miapp")
